93829c6d44da18ecfa38ea19a3f74f5aa0647883
akmroyal <ashutoshmaxxxx@gmail.com>
2024-09-24 01:01:09 +0530

    now we just added cloudinary(for file asst) & multer(for middleware check) upload,,, here we almost complete setup producation lvl setup :)

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,28 @@
+import { v2 as cloudnery } from "cloudinary"
+import { log } from "console"
+import fs from "fs"
+
+cloudnery.config({
+    cloud_name: "process.env.CLOUDINARY_CLOUD_NAME",
+    api_key: "process.env.CLOUDINARY_API_KEY",
+    api_secret: "process.env.CLOUDINARY_API_SECRET"
+})
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) {
+            return null
+        }
+        // upload the file on cloudinary
+        const res = await cloudnery.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+
+        console.log("File is uploaded on cloudinary :) - ", res.url);
+        return res;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // remove the locally saved temp file as the upload operation got failed
+        console.log("Error while uploading file on cloudinary - ", error);
+
+    }
+}
