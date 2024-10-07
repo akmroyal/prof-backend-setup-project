afd6f12c790cd5ee77ca26b94033597eb2b7820f
akmroyal <ashutoshmaxxxx@gmail.com>
2024-10-07 16:06:28 +0530

    Added comment, like, playlist and tweet models files

---
+++ src/models/like.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+const likeSchema = new Schema(
+    {
+        video: {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        },
+        comment: {
+            type: Schema.Types.ObjectId,
+            ref: "Comment"
+        },
+        tweet: {
+            type: Schema.Types.ObjectId,
+            ref: "Comment"
+        },
+        likedBy: {
+            type: Schema.Types.ObjectId,
+            ref: "User"
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+
+export const Like = mongoose.model("Like", likeSchema)
+
